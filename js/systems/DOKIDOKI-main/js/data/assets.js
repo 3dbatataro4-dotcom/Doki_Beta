4fb6a6916ee5528b2f456003af7c6f7e84ce3334
3dbatataro4-dotcom <3dbatataro4@gmail.com>
2026-03-05 19:59:51 +0800

    更新 js/systems/DOKIDOKI-main/js/data/assets.js via EZPage

---
+++ js/systems/DOKIDOKI-main/js/data/assets.js
@@ -0,0 +1,72 @@
+// 資源路徑與設定
+window.ASSETS = {
+    bg: {
+        gate: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E6%A0%A1%E9%96%80_1Week.jpg",
+        corridor: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E8%B5%B0%E5%BB%8A_1Week.jpg",
+        classroom: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E6%95%99%E5%AE%A4_1Week.jpg",
+        club_room: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E7%A4%BE%E5%9C%98%E6%95%99%E5%AE%A4_1Week.jpg",
+        home: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E7%8E%A9%E5%AE%B6%E6%88%BF%E9%96%93_1Week.jpg",
+        rooftop: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E5%A4%A9%E8%87%BA_1Week.jpg",
+        library: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E5%9C%96%E6%9B%B8%E9%A4%A8_1Week.jpg",
+        church: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E6%95%99%E5%A0%82_1Week.jpg",
+        planetarium: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E5%A4%A9%E6%96%87%E9%A4%A8_1Week.jpg",
+        amusement: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E9%81%8A%E6%88%B2%E5%9C%92_1Week.jpg",
+        aquarium: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E6%B5%B7%E7%94%9F%E9%A4%A8_1Week.jpg",
+        restaurant: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E9%A4%90%E5%BB%B3_1Week.jpg",
+        arcade: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BG/%E9%81%8A%E6%88%B2%E5%BB%B3_1Week.jpg"
+    },
+    char: {
+        peter: {
+            normal: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%BD%BC%E5%BE%97_%E6%99%AE%E9%80%9A.png",
+            smile: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%BD%BC%E5%BE%97_%E5%BE%AE%E7%AC%91.png",
+            surprise: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%BD%BC%E5%BE%97_%E9%A9%9A%E8%A8%9D%EF%BC%81.png",
+            confused: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%BD%BC%E5%BE%97_%E4%BD%A0%E8%AA%B0%E5%95%8A.png",
+            happy: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%BD%BC%E5%BE%97_%E8%B2%93%E8%B2%93%EF%BC%81.png",
+            horror: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%BD%BC%E5%BE%97_%E5%B4%A9%E5%A3%9E.png"
+        },
+        lanlan: {
+            normal: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E8%98%AD%E8%98%AD_%E6%99%AE%E9%80%9A.png",
+            bored: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E8%98%AD%E8%98%AD_%E4%B8%8D%E6%84%9F%E8%88%88%E8%B6%A3%E5%91%80.png",
+            happy: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E8%98%AD%E8%98%AD_%E5%BE%88%E9%96%8B%E5%BF%83.png",
+            angry: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E8%98%AD%E8%98%AD_%E4%BD%A0%E8%AA%B0%E5%91%80.png",
+            surprise: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E8%98%AD%E8%98%AD_%E9%A9%9A%E8%A8%9D.png",
+            horror: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E8%98%AD%E8%98%AD_%E5%B4%A9%E5%A3%9E.png"
+        },
+        ora: {
+            normal: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%A5%A7%E6%8B%89_%E6%99%AE%E9%80%9A.png",
+            surprise: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%A5%A7%E6%8B%89_%E9%A9%9A%E8%A8%9D.png",
+            ignore: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%A5%A7%E6%8B%89_%E7%9C%8B%E6%97%81%E9%82%8A.png",
+            smile: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%A5%A7%E6%8B%89_%E5%BE%AE%E7%AC%91.png",
+            horror: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/%E6%96%B0%E7%89%88%E7%AB%8B%E7%B9%AA/%E5%A5%A7%E6%8B%89_%E5%B4%A9%E5%A3%9E.png"
+        }
+    },
+    avatar: {
+        peter: "https://file.garden/aWe99vhwaGcNwkok/%E7%A0%B4%E9%A0%AD/%E5%BD%BC%E5%BE%97.png",
+        lanlan: "https://file.garden/aWe99vhwaGcNwkok/%E7%A0%B4%E9%A0%AD/%E8%98%AD%E8%98%AD.png",
+        ora: "https://file.garden/aWe99vhwaGcNwkok/%E7%A0%B4%E9%A0%AD/%E5%A5%A7%E6%8B%89.png",
+        player: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/ICON"
+    },
+    bgm: {
+        peace: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BGM/%E5%92%8C%E5%B9%B3%E7%B7%9A_%E8%AA%B2%E5%A0%82%E6%97%A5%E5%B8%B8%E6%99%82%E9%96%93.mp3",
+        club: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BGM/%E5%92%8C%E5%B9%B3%E7%B7%9A_%E7%A4%BE%E5%9C%98%E6%99%82%E9%96%93.mp3",
+        horror: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BGM/%E5%B4%A9%E5%A3%9E%E7%B7%9A_%E4%B8%8D%E5%A6%99%E4%BA%8B%E4%BB%B6.mp3",
+        home: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/BGM/%E5%92%8C%E5%B9%B3%E7%B7%9A_%E5%9B%9E%E5%AE%B6%E8%88%87%E6%97%A5%E5%B8%B8%E6%99%82%E9%96%93.mp3"
+    }
+};
+
+window.COLORS = {
+    peter: "var(--peter-color)",
+    lanlan: "var(--lanlan-color)",
+    ora: "var(--ora-color)",
+    system: "#666",
+    lynn: "var(--lynn-color)",
+    jornona: "var(--jornona-color)",
+    melas: "var(--melas-color)",
+    normal: "var(--normal-color)"
+};
+
+window.MOMENTS = [
+    { postId: "m_lanlan_1", id: "lanlan", name: "蘭蘭", avatar: "lanlan", content: "今天的火真好看呀！🔥🔥🔥 #燒烤 #藝術", img: "https://file.garden/aWe99vhwaGcNwkok/DOKIDOKI/ICON", time: "10分鐘前" },
+    { postId: "m_peter_1", id: "peter", name: "彼得", avatar: "peter", content: "今日消毒次數：44次。感覺還是有細菌。Shit。", time: "1小時前" },
+    { postId: "m_ora_1", id: "ora", name: "奧拉", avatar: "ora", content: "觀測日記：變量介入導致熵增。需持續監控。", time: "2小時前" }
+];
